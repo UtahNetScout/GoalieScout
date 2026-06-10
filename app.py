bb12b7286971ae3a2d2d62e50592b81e8e907b34
Ronald E. Taylor <ronnoki22@gmail.com>
2026-06-10 10:50:05 -0600

    Load goalie data and handle player ID input
    Load goalie data and display first player ID.
    
    Signed-off-by: Ronald E. Taylor <ronnoki22@gmail.com>

--- app.py
+++ app.py
@@ -22,7 +22,13 @@ if st.button("Generate AI Scouting Report"):
     if not player_id:
         st.warning("Please enter a Player ID.")
     else:
-        db = GoalieDatabase("data/sample_database.json")
+       db = GoalieDatabase("data/sample_database.json")
+
+data = db._load_data()
+st.write("Goalies loaded:", len(data.get("goalies", [])))
+st.write("First player ID:", data["goalies"][0]["player_id"] if data.get("goalies") else "None")
+
+goalie = db.get_goalie(player_id) db = GoalieDatabase("data/sample_database.json")
         goalie = db.get_goalie(player_id)
 
         if goalie is None:
